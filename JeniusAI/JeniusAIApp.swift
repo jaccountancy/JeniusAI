7322efd739b3ffacc69d3ddf485204a26e7a0c88
Jay Wilson <jay@jaccountancy.co.uk>
2026-05-05 10:33:39 +0100

    Initial Commit

---
+++ JeniusAI/JeniusAIApp.swift
@@ -0,0 +1,32 @@
+//
+//  JeniusAIApp.swift
+//  JeniusAI
+//
+//  Created by Jay Wilson on 05/05/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JeniusAIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ JeniusAITests/JeniusAITests.swift
@@ -0,0 +1,19 @@
+//
+//  JeniusAITests.swift
+//  JeniusAITests
+//
+//  Created by Jay Wilson on 05/05/2026.
+//
+
+import Testing
+@testable import JeniusAI
+
+struct JeniusAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
